6be123eefcdfffb7ce353fc2fb8b6be4a4abd789
Aleksandr Lysov <lsgsk@rambler.ru>
2016-01-29 10:49:48 +0300

    commit

---
+++ Android.NativeVyatka/Activities/BuriaEditActivity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Android.App;
+
+namespace NativeVyatkaAndroid
+{
+    /*[Activity(Label = "BurialDetailActivity", Theme = "@style/AppTheme", ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.KeyboardHidden)]            
+    public class BuriaEditActivity : BaseAppCompatActivity
+    {
+        //http://www.icons4android.com/ - иконки
+        protected override void OnCreate(Android.OS.Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+    }*/
+}
+
