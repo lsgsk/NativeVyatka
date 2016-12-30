4f32ead2b2c559532d2d4062467ede851dcea501
Alexsandr <lsgsk@rambler.ru>
2016-12-30 12:10:11 +0300

    Addded ios

---
+++ NativeVyatkaIOS/Controllers/UILoginController.cs
@@ -0,0 +1,13 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace NativeVyatkaIOS
+{
+    public partial class UILoginController : UIViewController
+    {
+        public UILoginController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
